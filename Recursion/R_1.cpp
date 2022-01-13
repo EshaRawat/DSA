ad21825779bb6c1ebba8ae6cd281bc1819f5f75b
Esha Rawat <32975000+EshaRawat@users.noreply.github.com>
2022-01-13 19:45:59 +0530

    Print n natural numbers

---
+++ Recursion/R_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void nat(int n){
+    if(n==0)
+        return;
+    nat(n-1);
+    cout<<n ;
+}
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    nat(n);
+    return 0;
+}
