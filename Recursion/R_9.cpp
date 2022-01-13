59df9082f0467887f6319fb553984dc898894905
Esha Rawat <32975000+EshaRawat@users.noreply.github.com>
2022-01-13 20:04:08 +0530

    1+1/3+1/9+1/27+...n

---
+++ Recursion/R_9.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern1(int n,int cp){
+    if(cp>=n)
+        return;
+    cout<<"1/"<<(int)pow(3,cp)<<" + ";
+    pattern1(n,cp+1);
+
+}
+int main(){
+    int n,cp=0;
+    cout<<"Enter n: ";
+    cin>>n;
+    pattern1(n,cp);
+    return 0;
+}
+
